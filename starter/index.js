638c6cfdb8dc45c0d60d6b378e638738e72b93ca
Garrison McMullen <mcmullengn@gmail.com>
2022-01-09 22:34:15 -0500

    Edit index.js

--- starter/index.js
+++ starter/index.js
@@ -5,36 +5,28 @@ var state = {};
 
 function init(shaders)
 {
-    state.programInfo = twgl.createProgramInfo(gl, [shaders['vs'], shaders['fs']]);
-
-    state.bufferInfo = twgl.primitives.createXYQuadBufferInfo(gl);
-
-    gl.enable(gl.BLEND);
-    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-    gl.clearColor(0.0,0.0,0.0,1.0);
-
-    requestAnimationFrame(render);
+  state.programInfo = twgl.createProgramInfo(gl, [shaders['vs'], shaders['fs']]);
+  state.bufferInfo = twgl.primitives.createXYQuadBufferInfo(gl);
+  requestAnimationFrame(render);
 }
 
 function render(time) { 
-    time *= 0.001; // milliseconds to seconds
+  time *= 0.001; // milliseconds to seconds
 
-    twgl.resizeCanvasToDisplaySize(gl.canvas, 1.0);
-    gl.viewport(0, 0, gl.canvas.clientWidth, gl.canvas.clientHeight);
+  twgl.resizeCanvasToDisplaySize(gl.canvas, 1.0);
+  gl.viewport(0, 0, gl.canvas.clientWidth, gl.canvas.clientHeight);
 
-    const uniforms = {
-        uTime: time,
-        uResolution: [gl.canvas.clientWidth, gl.canvas.clientHeight]
-    }
+  const uniforms = {
+      uTime: time,
+      uResolution: [gl.canvas.clientWidth, gl.canvas.clientHeight]
+  }
 
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.useProgram(state.programInfo.program);
+  twgl.setUniforms(state.programInfo, uniforms);
+  twgl.setBuffersAndAttributes(gl, state.programInfo, state.bufferInfo); 
+  twgl.drawBufferInfo(gl, state.bufferInfo);
 
-    gl.useProgram(state.programInfo.program);
-    twgl.setUniforms(state.programInfo, uniforms);
-    twgl.setBuffersAndAttributes(gl, state.programInfo, state.bufferInfo); 
-    twgl.drawBufferInfo(gl, state.bufferInfo);
-
-    requestAnimationFrame(render);
+  requestAnimationFrame(render);
 }
 
 // -------- FETCH FILES --------- //
